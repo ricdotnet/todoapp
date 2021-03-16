71a4f4e018dbc09c7b419c0ead0915b25b1756fb
Ricardo Rocha <me@rrocha.uk>
2021-03-16 23:52:47 +0000

    commit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,19 +2,42 @@
     <div class="text-center mt-10">
         Hello, :)<br>
         my name is {{ name }} and I am a software developer from {{ country }}.
+
+        <br><br>
+
+        {{ members }}
+
     </div>
+
 </template>
 
 <script>
 
+import axios from 'axios'
+
 export default {
     name: "Home",
 
-    data() {
+    data () {
         return {
             name: "Ricardo Rocha",
-            country: "Portugal"
+            country: "Portugal",
+
+            members: []
         }
+    },
+    mounted: function() {
+
+        this.getData()
+
+    },
+    methods: {
+
+        getData() {
+            axios.get("http://ricr.dev/api/members.php")
+                .then(response => (this.members = response.data))
+        }
+
     }
 }
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+//import axios from 'axios'
+//import VueAxios from 'vue-axios'
+
 import './assets/css/index.css'
 
 Vue.config.productionTip = false
+//Vue.use(VueAxios, axios)
 
 new Vue({
     router,
